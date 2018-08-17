ec126219237adf2964d160cd8059c001795f6a44
kansihk <42134462+kansihk@users.noreply.github.com>
2018-08-17 06:09:20 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,21 @@
+//first include the library
+#include<iostream>
+using namespace std;
+//writing the function
+int main ()
+{
+   cout << "Hello" <<endl;
+//declaring the variables
+int a;
+char b;
+float c;
+bool d;
+double e;
+//print the result
+cout << "size of int is " << sizeof(a) << endl;
+cout << "size of char is " << sizeof(b) <<endl;
+cout << "size of float is " << sizeof(c) <<endl;
+cout << "size of bool is " << sizeof(d) <<endl;
+cout << "size of double is " << sizeof(e) <<endl;
+return 0;
+}
