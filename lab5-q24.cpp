b6bff069d32f2bb2a7faf3319fec7c646c9abe40
kansihk <42134462+kansihk@users.noreply.github.com>
2018-08-31 12:46:22 +0530

    Add files via upload

---
+++ lab5-q24.cpp
@@ -0,0 +1,13 @@
+//program to print all alphabets from a to z.using while loop 
+#include<iostream>
+using namespace std;
+int main(){
+int i=97;
+while(i<=122)
+{
+   cout<< char(i) << endl;
+   i = i + 1;
+}
+  
+    return 0;
+}
---
+++ lab5-q26.cpp
@@ -0,0 +1,13 @@
+//to print all odd numbers between 1 to 100 using while loop
+#include<iostream>
+using namespace std;
+int main(){
+int i=1;
+while(i<=100)
+{
+   cout << int(i) << endl;
+    i+=2;
+}
+  
+    return 0;
+}
